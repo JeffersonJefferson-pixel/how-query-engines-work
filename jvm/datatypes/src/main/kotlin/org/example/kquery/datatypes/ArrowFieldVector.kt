4891cf47ce104e531050c4b037cb141500544a79
JeffersonJefferson-pixel <erenthewizard@gmail.com>
2024-10-03 12:52:05 +0700

    handle more data types in ArrowVectorBuilder and ArrowFieldVector

--- jvm/datatypes/src/main/kotlin/org/example/kquery/datatypes/ArrowFieldVector.kt
+++ jvm/datatypes/src/main/kotlin/org/example/kquery/datatypes/ArrowFieldVector.kt
@@ -1,14 +1,20 @@
 package org.example.kquery.datatypes
 
+import org.apache.arrow.vector.*
 import org.example.kquery.datatypes.ColumnVector
-import org.apache.arrow.vector.FieldVector
-import org.apache.arrow.vector.IntVector
 import org.apache.arrow.vector.types.pojo.ArrowType
 
 class ArrowFieldVector(private val field: FieldVector) : ColumnVector {
     override fun getType(): ArrowType {
         return when (field) {
+            is BitVector -> ArrowTypes.BooleanType
+            is TinyIntVector -> ArrowTypes.Int8Type
+            is SmallIntVector -> ArrowTypes.Int16Type
             is IntVector -> ArrowTypes.Int32Type
+            is BigIntVector -> ArrowTypes.Int64Type
+            is Float4Vector -> ArrowTypes.FloatType
+            is Float8Vector -> ArrowTypes.DoubleType
+            is VarCharVector -> ArrowTypes.StringType
             else -> throw IllegalStateException()
         }
     }
@@ -19,7 +25,21 @@ class ArrowFieldVector(private val field: FieldVector) : ColumnVector {
         }
 
         return when (field) {
+            is BitVector -> field.get(i) == 1
+            is TinyIntVector -> field.get(i)
+            is SmallIntVector -> field.get(i)
             is IntVector -> field.get(i)
+            is BigIntVector -> field.get(i)
+            is Float4Vector -> field.get(i)
+            is Float8Vector -> field.get(i)
+            is VarCharVector -> {
+                val bytes = field.get(i)
+                if (bytes == null ) {
+                    null
+                } else {
+                    String(bytes)
+                }
+            }
             else -> throw IllegalStateException()
         }
     }
--- jvm/datatypes/src/main/kotlin/org/example/kquery/datatypes/ArrowVectorBuilder.kt
+++ jvm/datatypes/src/main/kotlin/org/example/kquery/datatypes/ArrowVectorBuilder.kt
@@ -1,12 +1,33 @@
 package org.example.kquery.datatypes
 
+import org.apache.arrow.vector.*
 import org.example.kquery.datatypes.ColumnVector
-import org.apache.arrow.vector.FieldVector
-import org.apache.arrow.vector.IntVector
 
 class ArrowVectorBuilder(val fieldVector: FieldVector) {
     fun set(i: Int, value: Any?) {
         when (fieldVector) {
+            is TinyIntVector -> {
+                if (value == null) {
+                    fieldVector.setNull(i)
+                } else if (value is Number) {
+                    fieldVector.set(i, value.toByte())
+                } else if (value is String) {
+                    fieldVector.set(i, value.toByte())
+                } else {
+                    throw IllegalStateException()
+                }
+            }
+            is SmallIntVector -> {
+                if (value == null) {
+                    fieldVector.setNull(i)
+                } else if (value is Number) {
+                    fieldVector.set(i, value.toShort())
+                } else if  (value is String) {
+                    fieldVector.set(i, value.toShort())
+                } else {
+                    throw IllegalStateException()
+                }
+            }
             is IntVector -> {
                 if (value == null) {
                     fieldVector.setNull(i)
@@ -16,6 +37,48 @@ class ArrowVectorBuilder(val fieldVector: FieldVector) {
                     fieldVector.set(i, value.toInt())
                 } else {
                     throw IllegalStateException()
+                }
+            }
+            is BigIntVector -> {
+                if (value == null) {
+                    fieldVector.setNull(i)
+                } else if (value is Number) {
+                    fieldVector.set(i, value.toLong())
+                } else if (value is String) {
+                    fieldVector.set(i, value.toLong())
+                } else {
+                    throw IllegalStateException()
+                }
+            }
+            is Float4Vector -> {
+                if (value == null) {
+                    fieldVector.setNull(i)
+                } else if (value is Number) {
+                    fieldVector.set(i, value.toFloat())
+                } else if (value is String) {
+                    fieldVector.set(i, value.toFloat())
+                } else {
+                    throw IllegalStateException()
+                }
+            }
+            is Float8Vector -> {
+                if (value == null) {
+                    fieldVector.setNull(i)
+                } else if (value is Number) {
+                    fieldVector.set(i, value.toDouble())
+                } else if (value is String) {
+                    fieldVector.set(i, value.toDouble())
+                } else {
+                    throw IllegalStateException()
+                }
+            }
+            is VarCharVector -> {
+                if (value == null) {
+                    fieldVector.setNull(i)
+                } else if (value is ByteArray) {
+                    fieldVector.set(i, value)
+                } else {
+                    fieldVector.set(i, value.toString().toByteArray())
                 }
             }
         }
